261be3a9bb72524cf90a1f596903bfae06369195
solar <ikuralenok@gmail.com>
2017-08-26 00:50:54 +0300

    More refucktoring

--- find_edges/find_edges.cpp
+++ find_edges/find_edges.cpp
@@ -19,19 +19,6 @@ using namespace boost::numeric::ublas;
 
 typedef vector<double, std::vector<double>> vec;
 
-std::tuple<int, int, int, int> calc_coords(double alpha, double beta, double tau_0, double tau_1) {
-    double cs = cos(alpha);
-    double sn = sin(alpha);
-
-    int x_0 = tau_0 * cs + beta * sn;
-    int y_0 = tau_0 * sn - beta * cs;
-
-    int x_1 = tau_1 * cs + beta * sn;
-    int y_1 = tau_1 * sn - beta * cs;
-
-    return std::make_tuple(x_0, y_0, x_1, y_1);
-}
-
 double target_function(double alpha, double beta, double tau_0, double tau_1, const cv::Mat& image) {
     double result = 0.;
 
@@ -40,17 +27,19 @@ double target_function(double alpha, double beta, double tau_0, double tau_1, co
     double basisX = beta * bx;
     double basisY = beta * by;
 
-    for (int i = image.rows / 3; i < image.rows * 2/ 3; i++) {
-        for (int j = image.cols / 3; j < image.cols * 2 / 3; j++) {
+    double sum = 0;
+    double sum2 = 0;
+    double totalW = 0;
+    for (int i = 0; i < image.rows; i++) {
+        for (int j = i + 1; j < image.cols; j++) {
             double x = i - basisX;
             double y = j - basisY;
             // transfering to new basis
             const double projA = x * ax + y * ay;
             const double projB = x * bx + y * by;
-            double signum = projB >= 0 ? 1.0 : -1.0;
-
-            if ((projA - tau_0) * (projA - tau_1) > 0)
+            if (fabs(projB) < 1 || (projA - tau_0) * (projA - tau_1) > 0)
                 continue;
+            double signum = projB >= 0 ? 1.0 : -1.0;
 
             const double weight = -projB * projB;
 
@@ -61,70 +50,66 @@ double target_function(double alpha, double beta, double tau_0, double tau_1, co
             int mirrorI = (int)round(mirrorX);
 
             if (mirrorI < 0 || mirrorJ < 0 || mirrorI >= image.rows || mirrorJ >= image.cols)
-                return 0;
+                continue;
 
             double proBabOrig = image.at<double>(i, j);
             double proBabMirror = image.at<double>(mirrorI, mirrorJ);
             double logOdds = log(proBabOrig) - log(proBabMirror);
-            result += signum * exp(weight * 2) * logOdds;
+            static double maxOdds = 0;
+            if (fabs(logOdds) > maxOdds) {
+                maxOdds = fabs(logOdds);
+                std::cout << logOdds << std::endl;
+            }
+            double w = exp(weight / 2);
+            sum += w * signum * logOdds;
+            sum2 += w * logOdds * logOdds;
+            totalW += w;
+            result += signum * w * logOdds;
         }
     }
 
-    return result;
+    double D = totalW > 0 ? (sum2 - sum * sum / totalW) : 0;
+    if (D > 0) {
+//        if (D > 1)
+//            std::cout << "Hello";
+        return result - D;
+//        if (result > 9)
+//            target_function(alpha, beta, tau_0, tau_1, image);
+//        return result;
+    }
+    return 0;
 }
 
 void optimize(const cv::Mat& img, const cv::Mat& img_norm) {
-    const size_t num_instances = 100;
+    const size_t num_instances = 360;
 
     int side = std::max(img_norm.cols, img_norm.rows);
-
-    double alphas[num_instances];
-    double betas[2 * side];
-
-    double tau_0[2 * side];
-    double tau_1[2 * side];
-
-    for (size_t i = 0; i < num_instances; i++) {
-        alphas[i] = 2. * M_PI * i / num_instances;
-    }
-
-    for (int i = -side; i < side; i++) {
-        betas[i + side] = i;
-        tau_0[i + side] = i;
-        tau_1[i + side] = i;
-    }
-
-    double max_value = -DBL_MAX;
-
     size_t counter = 0;
 
     for (size_t i = 0; i < num_instances; i++) {
         for (size_t j = 0; j < side * 2; j++) {
-            for (size_t k = 0; k < side * 2; k++) {
-                for (size_t l = 0; l < side * 2; l++) {
-                    auto tpl = calc_coords(alphas[i], betas[j], tau_0[k], tau_1[l]);
-                    int x_0 = std::get<0>(tpl);
-                    int y_0 = std::get<1>(tpl);
-                    int x_1 = std::get<2>(tpl);
-                    int y_1 = std::get<3>(tpl);
+            for (size_t k = 0; k < side; k++) {
+                for (size_t l = k; l < side; l++) {
+                    double alpha = 2. * M_PI * i / num_instances;
+                    double beta = j - side;
+                    double t_0 = k;
+                    double t_1 = l;
+                    double value = target_function(alpha, beta, t_0, t_1, img_norm);
 
-                    if (x_0 < img.rows / 3 || x_0 > 2 * img.rows / 3
-                     || y_0 < img.cols / 3 || y_0 > 2 * img.cols / 3
-                     || x_1 < img.rows / 3 || x_1 > 2 * img.rows / 3
-                     || y_1 < img.cols / 3 || y_1 > 2 * img.cols / 3)
-                    {
-                        continue;
-                    }
+                    if (value > 5) {
+                        double ax = cos(alpha), ay = sin(alpha);
+                        double bx = cos(alpha + CV_PI / 2), by = sin(alpha + CV_PI / 2);
+                        
+                        int x_0 = (int) round(t_0 * ax + beta * bx);
+                        int y_0 = (int) round(t_0 * ay + beta * by);
 
-                    double value = target_function(alphas[i], betas[j], tau_0[k], tau_1[l], img_norm);
-
-                    if (value > max_value) {
-                        max_value = value;
+                        int x_1 = (int) round(t_1 * ax + beta * bx);
+                        int y_1 = (int) round(t_1 * ay + beta * by);
 
                         std::cout << "score " << value << " ";
                         std::cout << x_0 << " " << y_0 << " " << x_1 << " " << y_1 << std::endl;
-                        std::cout << "alpha " << alphas[i] << " beta " << betas[j] << " tau0 "
-                            << tau_0[k] << " tau1 " << tau_1[l] << std::endl;
+                        std::cout << "alpha " << alpha << " beta " << beta << " tau0 "
+                                  << t_0 << " tau1 " << t_1 << std::endl;
 
                         cv::Mat copy_img = img.clone();
                         cv::line(copy_img, cv::Point(y_0, x_0), cv::Point(y_1, x_1), cv::Scalar(255, 0, 0));
@@ -152,8 +137,8 @@ int main() {
     cv::Mat img_gray;
     cv::Mat img_norm;
 
-    img = imread("../results/hog_10K.png");
-    resize(img, img, cv::Size(100, 100));
+    img = imread("../results/hog_10K_small.png");
+//    resize(img, img, cv::Size(100, 100));
     cvtColor(img, img_gray, COLOR_BGR2GRAY);
 
 //    for (int i = 0; i < img.rows; i++) {
