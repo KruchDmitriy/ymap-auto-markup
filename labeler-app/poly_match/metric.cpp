efaa2b9e794f83c86b115fa5f6c43bf68f834eba
KruchDmitriy <kruch.dmitriy@gmail.com>
2018-01-10 23:03:44 +0300

    cpp cli v0.1

--- labeler-app/poly_match/metric.cpp
+++ labeler-app/poly_match/metric.cpp
@@ -6,7 +6,6 @@ enum Args {
     HELP,
     REAL,
     GEN,
-    OUT,
     MIN_SHIFT,
     MAX_SHIFT,
     MIN_THETA,
@@ -20,17 +19,17 @@ enum Args {
 
 static void small_help() {
     std::cout <<
-    "usage: ./metric [-h] --real REAL --gen GEN --out OUT [--min_shift MIN_SHIFT]" << std::endl <<
+    "usage: ./metric [-h] --real REAL --gen GEN [--min_shift MIN_SHIFT]" << std::endl <<
     "             [--max_shift MAX_SHIFT] [--min_theta MIN_THETA]" << std::endl <<
     "             [--max_theta MAX_THETA] [--min_scale MIN_SCALE]" << std::endl <<
     "             [--max_scale MAX_SCALE] [--grid_step GRID_STEP]" << std::endl <<
     "             [--desc_num_steps DESC_NUM_STEPS] [--learn_rate LEARN_RATE]" << std::endl <<
-    "./metric: error: the following arguments are required: --real, --gen, --out" << std::endl;
+    "./metric: error: the following arguments are required: --real, --gen" << std::endl;
 }
 
 static void help() {
     std::cout <<
-    "usage: metric.py [-h] --real REAL --gen GEN --out OUT [--min_shift MIN_SHIFT]" << std::endl <<
+    "usage: metric.py [-h] --real REAL --gen GEN [--min_shift MIN_SHIFT]" << std::endl <<
     "             [--max_shift MAX_SHIFT] [--min_theta MIN_THETA]" << std::endl <<
     "             [--max_theta MAX_THETA] [--min_scale MIN_SCALE]" << std::endl <<
     "             [--max_scale MAX_SCALE] [--grid_step GRID_STEP]" << std::endl <<
@@ -40,8 +39,7 @@ static void help() {
     "  --real REAL           path to json file with list polygons [poly1, poly2, poly3]" << std::endl <<
     "  --gen GEN             path to json file with list generated polygons [gen_poly1," << std::endl <<
     "                        gen_poly2, gen_poly3]gen_poly1 will be compared with" << std::endl <<
-    "                        poly1 etc" << std::endl <<
-    "  --out OUT             output file name" << std::endl << std::endl <<
+    "                        poly1 etc" << std::endl << std::endl <<
     "optimization parameters for grid search:" << std::endl <<
     "  --min_shift MIN_SHIFT" << std::endl <<
     "  --max_shift MAX_SHIFT" << std::endl <<
@@ -55,12 +53,22 @@ static void help() {
     "  --learn_rate LEARN_RATE" << std::endl;
 }
 
+struct ParsedArgs {
+    char* file_real;
+    char* file_gen;
+    OptimizationParams params;
+
+    ParsedArgs(char* file_real, char* file_gen, OptimizationParams params)
+    : file_real(file_real)
+    , file_gen(file_gen)
+    , params(params) {}
+};
+
 static ParsedArgs* parse_params(int argc, char** argv) {
     std::unordered_map<std::string, int> mapping = {
         {"-h", HELP}, {"--help", HELP},
         {"--real", REAL},
         {"--gen", GEN},
-        {"--out", OUT},
         {"--min_shift", MIN_SHIFT},
         {"--max_shift", MAX_SHIFT},
         {"--min_theta", MIN_THETA},
@@ -74,85 +82,76 @@ static ParsedArgs* parse_params(int argc, char** argv) {
 
     OptimizationParamsBuilder builder;
     int required_completed = 0;
+    char* file_real;
+    char* file_gen;
 
-    for (int i = 1; i < argc; i++) {
+    for (int i = 1; i < argc; i += 2) {
         switch (mapping[argv[i]]) {
             case HELP:
                 help();
                 return nullptr;
             case REAL:
-                std::cout << "    REAL," << std::endl;
+                file_real = argv[i + 1];
                 required_completed++;
                 break;
             case GEN:
-                std::cout << "    GEN,"<< std::endl;
-                required_completed++;
-                break;
-            case OUT:
-                std::cout << "    OUT,"<< std::endl;
+                file_gen = argv[i + 1];
                 required_completed++;
                 break;
             case MIN_SHIFT: {
                 double min_shift = atof(argv[i + 1]);
                 builder.set_min_shift(min_shift);
-                i++;
                 break;
             }
             case MAX_SHIFT: {
                 double max_shift = atof(argv[i + 1]);
                 builder.set_max_shift(max_shift);
-                i++;
                 break;
             }
             case MIN_THETA: {
                 double min_theta = atof(argv[i + 1]);
                 builder.set_min_theta(min_theta);
-                i++;
                 break;
             }
             case MAX_THETA: {
                 double max_theta = atof(argv[i + 1]);
                 builder.set_max_theta(max_theta);
-                i++;
                 break;
             }
             case MIN_SCALE: {
                 double min_scale = atof(argv[i + 1]);
                 builder.set_min_scale(min_scale);
-                i++;
                 break;
             }
             case MAX_SCALE: {
                 double max_scale = atof(argv[i + 1]);
                 builder.set_max_scale(max_scale);
-                i++;
                 break;
             }
             case GRID_STEP: {
                 int grid_step = atoi(argv[i + 1]);
                 builder.set_grid_step(grid_step);
-                i++;
                 break;
             }
             case DESC_NUM_STEPS: {
                 int desc_num_steps = atoi(argv[i + 1]);
                 builder.set_desc_num_steps(desc_num_steps);
-                i++;
                 break;
             }
             case LEARN_RATE: {
                 double learn_rate = atof(argv[i + 1]);
                 builder.set_learn_rate(learn_rate);
-                i++;
                 break;
             }
         }
     }
 
-    if (required_completed < 3) {
+    if (required_completed < 2) {
         small_help();
         return nullptr;
     }
+
+    return new ParsedArgs(file_real, file_gen, builder.build());
 }
 
 int main(int argc, char** argv) {
